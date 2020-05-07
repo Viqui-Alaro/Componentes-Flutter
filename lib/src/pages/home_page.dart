c4b1491bbf86d070117830a616ed35cba5154024
Viqui-Alaro <viquialaro241@gmail.com>
2020-05-06 22:11:37 -0400

    implementacion de metodo para la lectura de iconos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/providers/menu_provider.dart';
@@ -37,7 +38,7 @@ class HomePage extends StatelessWidget {
    data.forEach((opt){
      final widgetTemp = ListTile(
        title: Text(opt['texto']),
-       leading: Icon(Icons.account_circle,color: Colors.blue),
+       leading: getIcon(opt['icon']),
        trailing: Icon(Icons.keyboard_arrow_right,color: Colors.blue),
         onTap: (){
 
