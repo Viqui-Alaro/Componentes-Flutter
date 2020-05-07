1756fed07a2c6659e3a7271891541640ef60ec76
Viqui-Alaro <viquialaro241@gmail.com>
2020-05-06 21:31:55 -0400

    listado consumiendo el json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -16,22 +16,37 @@ class HomePage extends StatelessWidget {
       
      Widget _lista() {
        
-       print(menuProvider.opciones);
-       return ListView(
-          children: _listaItems(),
+       return FutureBuilder(
+         future: menuProvider.cargarData(),
+         builder: (context,AsyncSnapshot<List<dynamic>> snapshot){
+              return ListView(
+          children: _listaItems(snapshot.data)
+          );
+         },
        );
+
+
+
      }
 
 
- List<Widget> _listaItems() {
-   return[
-     ListTile(title: Text("Hola Viqui ")),
-     Divider(),
-     ListTile(title: Text("Hola Alaro ")),
-     Divider(),
-     ListTile(title: Text("Hola Mamani ")),
+ List<Widget> _listaItems(List<dynamic> data) {
+   
+   final List<Widget> opciones=[];
 
-   ];
+   data.forEach((opt){
+     final widgetTemp = ListTile(
+       title: Text(opt['texto']),
+       leading: Icon(Icons.account_circle,color: Colors.blue),
+       trailing: Icon(Icons.keyboard_arrow_right,color: Colors.blue),
+        onTap: (){
+
+        },
+     );
+    opciones..add(widgetTemp)
+            ..add(Divider());
+   });
+  return opciones;
  }
 }
  
